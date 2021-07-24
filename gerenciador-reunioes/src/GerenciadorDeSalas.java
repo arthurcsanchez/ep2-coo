5a02ac9ffa454b6396c59aad82d90a07b5b60535
Arthur Sanchez <arthurcsanchez@usp.br>
2021-07-24 19:04:45 -0300

    Cria métodos do enunciado e cria classes Reserva e Sala

--- gerenciador-reunioes/src/GerenciadorDeSalas.java
+++ gerenciador-reunioes/src/GerenciadorDeSalas.java
@@ -1,2 +1,25 @@
+import java.time.*;
+import java.util.*;
+
 public class GerenciadorDeSalas {
+
+	/* construtor */
+	public GerenciadorDeSalas() {}
+
+	/* métodos do enunciado */
+	public void adicionaSalaChamada(String nomeSala, int capacidadeMaxima, String descricao) {}
+
+	public void removeSalaChamada(String nomeSala) {}
+
+	public List<Sala> listaDeSalas() {}
+
+	public void adicionaSala(Sala novaSala) {}
+
+	public Reserva reservaSalaChamada(String nomeSala, LocalDateTime dataInicial, LocalDateTime dataFinal) {}
+
+	public void cancelaReserva(Reserva cancelada) {}
+
+	public Collection<Reserva> reservaParaSala(String nomeSala) {}
+
+	public void imprimeReservasDaSala(String nomeSala) {}
 }
--- gerenciador-reunioes/src/MarcadorDeReuniao.java
+++ gerenciador-reunioes/src/MarcadorDeReuniao.java
@@ -1,2 +1,15 @@
+import java.time.*;
+import java.util.*;
+
 public class MarcadorDeReuniao {
+
+	/* construtor */
+	public MarcadorDeReuniao() {}
+
+	/* métodos do enunciado */
+	public void marcarReuniaoEntre(LocalDate dataInicial, LocalDate dataFinal, Collection<String> listaDeParticipantes) {}
+
+	public void indicaDisponibilidadeDe(String participante, LocalDateTime inicio, LocalDateTime fim) {}
+
+	public void mostrarSobreposicao() {}
 }
