f781e7ea6e9b2258cd526acc4bfda5ff1e1058c9
Arthur Sanchez <arthurcsanchez@usp.br>
2021-07-27 19:25:52 -0300

    Implementa campos e métodos da classe Sala

--- gerenciador-reunioes/src/Sala.java
+++ gerenciador-reunioes/src/Sala.java
@@ -1,2 +1,38 @@
 public class Sala {
+
+	private final String nome;
+	private final String local;
+	private int capacidade;
+	private String observacoes;
+
+	public Sala(String nome, String local, int capacidade, String observacoes) {
+		this.nome = nome;
+		this.local = local;
+		this.capacidade = capacidade;
+		this.observacoes = observacoes;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public int getCapacidade() {
+		return capacidade;
+	}
+
+	public void setCapacidade(int novo) {
+		capacidade = novo;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String novo) {
+		observacoes = novo;
+	}
 }
