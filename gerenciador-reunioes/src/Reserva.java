bdb330e243bc8e8add1c41fe32eb0dc361bfc3d3
Arthur Sanchez <arthurcsanchez@usp.br>
2021-07-28 12:52:54 -0300

    Implementa classe Reserva

--- gerenciador-reunioes/src/Reserva.java
+++ gerenciador-reunioes/src/Reserva.java
@@ -1,2 +1,26 @@
+import java.time.LocalDateTime;
+
 public class Reserva {
+
+	private final Sala sala;
+	private final LocalDateTime inicio;
+	private final LocalDateTime fim;
+
+	public Reserva(Sala sala, LocalDateTime inicio, LocalDateTime fim) {
+		this.sala = sala;
+		this.inicio = inicio;
+		this.fim = fim;
+	}
+
+	public Sala sala() {
+		return sala;
+	}
+
+	public LocalDateTime inicio() {
+		return inicio;
+	}
+
+	public LocalDateTime fim() {
+		return fim;
+	}
 }
